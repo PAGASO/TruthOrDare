6fe742997c03bf86e009d39c6e58b2f51863a450
Administrator <administrator@Administrators-iMac.local>
2011-11-25 13:40:30 +0530

    Initial Commit

---
+++ TruthOrDareTests/TruthOrDareTests.h
@@ -0,0 +1,13 @@
+//
+//  TruthOrDareTests.h
+//  TruthOrDareTests
+//
+//  Created by Administrator on 25/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TruthOrDareTests : SenTestCase
+
+@end
---
+++ TruthOrDareTests/TruthOrDareTests.m
@@ -0,0 +1,32 @@
+//
+//  TruthOrDareTests.m
+//  TruthOrDareTests
+//
+//  Created by Administrator on 25/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TruthOrDareTests.h"
+
+@implementation TruthOrDareTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TruthOrDareTests");
+}
+
+@end
